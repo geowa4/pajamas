689e8a8ec82d0e7c24c7cd15a502adc137c1f2f7
George Adams IV <g.w.adams.iv@gmail.com>
2012-10-18 23:06:47 -0400

    mock the script test

--- test/script/script-test.js
+++ test/script/script-test.js
@@ -1,4 +1,50 @@
-/*global qjax:true module:true asyncTest:true ok:true strictEqual:true start:true*/
+/*global qjax:true FakeXHR:true module:true test:true asyncTest:true ok:true strictEqual:true deepEqual:true start:true*/
+module('fake xhr', {
+    setup    : function () {
+      FakeXHR.setup()
+    }
+  , teardown : function () {
+      FakeXHR.restore()
+    }
+})
+
+test('infer script data type', 1, function () {
+  qjax({
+      url : 'script-test.js'
+  })
+
+  deepEqual(FakeXHR.instance.methodCallArgs('setRequestHeader', 0), [
+        'Accept'
+      , 'text/javascript, application/javascript,' +
+        ' application/ecmascript, application/x-ecmascript'
+    ]
+    , 'Accepts header defaults to script')
+})
+
+test('check method calls', 10, function () {
+  qjax({
+      url      : 'script-test.js'
+    , dataType : 'script'
+    , data     : {
+        foo : 'bar'
+      }
+  })
+
+  strictEqual(FakeXHR.instance.methodCallCount('open'), 1, 'open called')
+  strictEqual(FakeXHR.instance.methodCallArgs('open', 0).length, 3, 'open called with 3 args')
+  deepEqual(FakeXHR.instance.methodCallArgs('open', 0), ['GET', 'script-test.js?foo=bar', true], 'check open args')
+
+  strictEqual(FakeXHR.instance.methodCallCount('setRequestHeader'), 3, 'setRequestHeader called 3x')
+  deepEqual(FakeXHR.instance.methodCallArgs('setRequestHeader', 0), ['Accept', 'text/javascript, application/javascript, application/ecmascript, application/x-ecmascript'], 'Accepts header is set')
+  deepEqual(FakeXHR.instance.methodCallArgs('setRequestHeader', 1), ['X-Requested-With', 'XMLHttpRequest'], 'X-Requested-With header is set')
+  deepEqual(FakeXHR.instance.methodCallArgs('setRequestHeader', 2), ['Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8'], 'Content-Type header is set')
+
+  strictEqual(FakeXHR.instance.methodCallCount('send'), 1, 'send called')
+  strictEqual(FakeXHR.instance.methodCallArgs('send', 0).length, 1, 'send called with 1 args')
+  deepEqual(FakeXHR.instance.methodCallArgs('send', 0), [null], 'send called with null')
+})
+
+
 module('valid GET')
 
 asyncTest('GET value', 3, function () {
