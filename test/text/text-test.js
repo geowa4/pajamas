0b4bb1ffb6da53fafc9e71116d3fdecf2206dec7
George Adams IV <g.w.adams.iv@gmail.com>
2012-10-20 00:07:06 -0400

    add xhr mock to text tests

--- test/text/text-test.js
+++ test/text/text-test.js
@@ -1,4 +1,52 @@
 /*global qjax:true module:true asyncTest:true ok:true strictEqual:true start:true*/
+module('fake xhr', {
+    setup    : function () {
+      FakeXHR.instance = null
+    }
+})
+
+test('infer text data type', 1, function () {
+  qjax({
+      url : 'text-test-value.txt'
+    , xhr : function () {
+        return new FakeXHR()
+      }
+  })
+
+  deepEqual(FakeXHR.instance.methodCallArgs('setRequestHeader', 0), [
+        'Accept'
+      , 'text/plain'
+    ]
+    , 'Accepts header defaults to script')
+})
+
+test('check method calls', 10, function () {
+  qjax({
+      url      : 'text-test-value.htm'
+    , dataType : 'text'
+    , data     : {
+        foo : 'bar'
+      }
+    , xhr      : function () {
+        return new FakeXHR()
+      }
+  })
+
+  strictEqual(FakeXHR.instance.methodCallCount('open'), 1, 'open called')
+  strictEqual(FakeXHR.instance.methodCallArgs('open', 0).length, 3, 'open called with 3 args')
+  deepEqual(FakeXHR.instance.methodCallArgs('open', 0), ['GET', 'script-test.js?foo=bar', true], 'check open args')
+
+  strictEqual(FakeXHR.instance.methodCallCount('setRequestHeader'), 3, 'setRequestHeader called 3x')
+  deepEqual(FakeXHR.instance.methodCallArgs('setRequestHeader', 0), ['Accept', 'text/plain'], 'Accepts header is set')
+  deepEqual(FakeXHR.instance.methodCallArgs('setRequestHeader', 1), ['X-Requested-With', 'XMLHttpRequest'], 'X-Requested-With header is set')
+  deepEqual(FakeXHR.instance.methodCallArgs('setRequestHeader', 2), ['Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8'], 'Content-Type header is set')
+
+  strictEqual(FakeXHR.instance.methodCallCount('send'), 1, 'send called')
+  strictEqual(FakeXHR.instance.methodCallArgs('send', 0).length, 1, 'send called with 1 args')
+  deepEqual(FakeXHR.instance.methodCallArgs('send', 0), [null], 'send called with null')
+})
+
+
 module('valid GET')
 
 asyncTest('GET text', 4, function () {
