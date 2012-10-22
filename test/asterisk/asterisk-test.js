c2147071153c329753d950078e2cc90d5fbf3286
George Adams IV <g.w.adams.iv@gmail.com>
2012-10-22 16:40:00 -0400

    test default parser resolves with xhr instead of the value.

---
+++ test/asterisk/asterisk-test.js
@@ -0,0 +1,34 @@
+/*global qjax:true FakeXHR:true module:true asyncTest:true test ok:true deepEqual:true start:true*/
+module('fake xhr', {
+    setup : function () {
+      FakeXHR.instance = null
+    }
+})
+
+test('Accept header when dataType is set to *', 1, function () {
+  qjax({
+      url      : 'asterisk-test.html'
+    , dataType : '*'
+    , xhr      : function () {
+        return new FakeXHR()
+      }
+  })
+
+  deepEqual(FakeXHR.instance.methodCallArgs('setRequestHeader', 0), ['Accept', 'text/javascript, text/html, application/xml, text/xml, */*'], 'Accepts header is set')
+})
+
+
+module('deferred resolution')
+
+asyncTest('resolved with the xhr', 1, function () {
+  qjax({
+      url      : 'asterisk-test.html'
+    , dataType : '*'
+  }).then(function (response) {
+    ok(response instanceof (window.XMLHttpRequest || window.ActiveXObject), 'resolved with the xhr')
+    start()
+  }, function (reason) {
+    ok(false, 'deferred was rejected')
+    start()
+  })
+})
