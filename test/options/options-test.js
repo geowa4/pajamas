ffd55dfca7e412d91066dcae749a1a5f5d13d80e
George Adams IV <g.w.adams.iv@gmail.com>
2013-04-20 18:55:12 -0400

    formatting options-test

--- test/options/options-test.js
+++ test/options/options-test.js
@@ -4,17 +4,16 @@ module('success')
 
 asyncTest('success handler can be null', 1, function () {
   pj({
-      url     : '../json/json-test.json'
-    , success : null
+    url     : '../json/json-test.json'
+  , success : null
   })
   .then(function (value) {
-      ok(value != null, 'value is returned')
-      start()
-    }
-  , function () {
-      ok(false, 'promise was rejected')
-      start()
-    })
+    ok(value != null, 'value is returned')
+    start()
+  }, function () {
+    ok(false, 'promise was rejected')
+    start()
+  })
 })
 
 asyncTest('success handler fires first', 3, function () {
@@ -22,36 +21,34 @@ asyncTest('success handler fires first', 3, function () {
     , originalValue
 
   pj({
-      url     : '../json/json-test.json'
-    , success : function (value) {
-        check = true
-        originalValue = value
-      }
+    url     : '../json/json-test.json'
+  , success : function (value) {
+      check = true
+      originalValue = value
+    }
   })
   .then(function (value) {
-      ok(check, 'success handler already ran')
-      strictEqual(value, originalValue, 'original value is maintained')
-      start()
-    }
-  , function () {
-      ok(false, 'promise was rejected')
-      start()
-    })
+    ok(check, 'success handler already ran')
+    strictEqual(value, originalValue, 'original value is maintained')
+    start()
+  }, function () {
+    ok(false, 'promise was rejected')
+    start()
+  })
 
   pj({
-      url     : '../json/json-test.json'
-    , success : function () {
-        throw new Error('failed')
-      }
+    url     : '../json/json-test.json'
+  , success : function () {
+      throw new Error('failed')
+    }
   })
   .then(function () {
-      ok(false, 'promise was resolved')
-      start()
-    }
-  , function (reason) {
-      strictEqual(reason.message, 'failed', 'promise became rejected')
-      start()
-    })
+    ok(false, 'promise was resolved')
+    start()
+  }, function (reason) {
+    strictEqual(reason.message, 'failed', 'promise became rejected')
+    start()
+  })
 })
 
 
@@ -59,51 +56,48 @@ module('error')
 
 asyncTest('error handler can be null', 1, function () {
   pj({
-      url   : 'this-does-not-exist.json'
-    , error : null
+    url   : 'this-does-not-exist.json'
+  , error : null
   })
   .then(function () {
-      ok(false, 'promise was resolved')
-      start()
-    }
-  , function (reason) {
-      ok(reason != null, 'reason given')
-      start()
-    })
+    ok(false, 'promise was resolved')
+    start()
+  }, function (reason) {
+    ok(reason != null, 'reason given')
+    start()
+  })
 })
 
 asyncTest('error handler fires first', 3, function () {
   var check = false
     , originalReason
   pj({
-      url   : 'this-does-not-exist.json'
-    , error : function (reason) {
-        check = true
-        originalReason = reason
-      }
+    url   : 'this-does-not-exist.json'
+  , error : function (reason) {
+      check = true
+      originalReason = reason
+    }
   })
   .then(function () {
-      ok(false, 'promise was resolved')
-      start()
-    }
-  , function (reason) {
-      ok(check, 'error handler already ran')
-      strictEqual(reason, originalReason, 'original reason is maintained')
-      start()
-    })
+    ok(false, 'promise was resolved')
+    start()
+  }, function (reason) {
+    ok(check, 'error handler already ran')
+    strictEqual(reason, originalReason, 'original reason is maintained')
+    start()
+  })
 
   pj({
-      url   : 'this-does-not-exist.json'
-    , error : function () {
-        return true
-      }
+    url   : 'this-does-not-exist.json'
+  , error : function () {
+      return true
+    }
   })
   .then(function (value) {
-      strictEqual(value, true, 'promise became resolved')
-      start()
-    }
-  , function () {
-      ok(false, 'promise was rejected')
-      start()
-    })
+    strictEqual(value, true, 'promise became resolved')
+    start()
+  }, function () {
+    ok(false, 'promise was rejected')
+    start()
+  })
 })
